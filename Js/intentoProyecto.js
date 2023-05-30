075094ebf2c98652d449070cbcf7e4dcc5d80d82
Agustin Defilippi <defilippiagustin@hotmail.com>
2023-05-30 00:01:46 -0300

    avanzando

--- Js/intentoProyecto.js
+++ Js/intentoProyecto.js
@@ -7,8 +7,65 @@ const viajes = [{destino:"salta",duracion:"5 dias",boleto:"ida y vuelta",hospeda
 {destino:"santa cruz",duracion:"4 dias",boleto:"ida y vuelta",hospedaje:"Cabañas el Arriero",precio:100000,categoria:"alta",zona:"sur"},
 {destino:"cafayate",duracion:"5 dias",boleto:"ida y vuelta",hospedaje:"Suites la Ferrere",precio:185300,categoria:"oferta",zona:"sur"}];
 
+const eleccionViaje = () =>{
+    
+    let paqueteViaje = prompt("ingrese que el destino donde usted quiere viajar dentro del pais"+"\n\n"+"*Norte"+"\n"+"*Centro"+"\n"+"*Sur").toLowerCase();
 
-let PaqueteDeViaje = eleccionViaje()
+    const zonaViaje = viajes.filter(elementos => elementos.zona === paqueteViaje);
+   /*  console.log(zonaViaje); */
+    if(zonaViaje.length > 0){
+        let salida1= "";
+
+        zonaViaje.forEach(elemento =>{(salida1+=`Destino: ${elemento.destino}.\n Duracion: ${elemento.duracion}. \n Pasaje: ${elemento.boleto}. \n Hospedaje: ${elemento.hospedaje}. \n Precio :  $${elemento.precio}. \n Zona: ${elemento.zona}. \n\n`)});
+
+        alert(`Paquetes de viaje disponibles en zona (${paqueteViaje}) del pais. \n\n ${salida1}`)
+
+        let destinoZonaViaje = prompt(`Ingrese que paquete de viaje desea contratar, eligiendo acorde a la zona (${paqueteViaje}) que ingreso.`).toLocaleLowerCase();
+
+        const destinoViajeFinal = zonaViaje.find(elemento => elemento.destino === destinoZonaViaje);
+
+        /*  console.log(destinoViajeFinal); */
+        
+        let salida2="";
+
+        for (const elemento in destinoViajeFinal) {
+            salida2+= elemento+":"+" "+destinoViajeFinal[elemento]+`\n`;
+        }
+
+        alert(`Felicitaciones usted ha elegido con exito (${destinoZonaViaje}) como paquete de viaje!!! \n\n ${salida2}`);
+
+        let resultado =  destinoViajeFinal;
+        return resultado
+
+    }else{
+        alert("Ingreso datos incorrectos")
+    }
+    
+}
+
+const efectivo = (valor) =>{
+    valor * 0.8
+}
+
+
+const vuelo = [];
+
+const viaje = eleccionViaje();
+
+vuelo.push(viaje)
+
+console.log(viaje);
+console.log(vuelo);
+
+let total = prompt(`ingrese forma de pago para conocer el monto final del paquete contratado \n\n *Efectivo\n *3 o 6 pagos \n *Ahora 12`).toLocaleLowerCase();
+
+if(total === "efectivo"){
+   
+  const total1 = vuelo.map(elemento => elemento.precio * 0.8);
+  console.log(total1);
+}
+
+
 
 
 
